31c8fd905076833cac03f064862491c626582326
Martin <martin.velinov24@hotmail.com>
2022-12-23 22:18:24 +0100

    server start

--- server/app.js
+++ server/app.js
@@ -30,5 +30,10 @@ app.use('/profile-images', express.static('upload/users'));
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/recipes', recipesRouter);
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
 
 module.exports = app;
